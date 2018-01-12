314603ee8bb766663b274480c75725d282aa8840
Barylo Aleks <baryloaleks@gmail.com>
2018-01-12 14:27:49 +0200

    script for menu-items darkened

--- dist/js/main.js
+++ dist/js/main.js
@@ -11,3 +11,24 @@ rightSide.addEventListener('mouseover', function() {
     rightText.classList.add('active') });
 rightSide.addEventListener('mouseout', function() {
     rightText.classList.remove('active') });
+
+let menuLinks = document.querySelectorAll('.main-menu-link'),
+    menuItems = document.querySelectorAll('.main-menu-item');
+
+    
+menuItems.forEach(function(item, i, arr){
+    item.addEventListener('mouseover', function(event){
+        menuLinks.forEach(function(item, i, arr){
+            item.classList.add('darkened');
+        });
+        this.querySelector('.main-menu-link').classList.remove('darkened');
+    })
+    item.addEventListener('mouseout', function(event){
+        menuLinks.forEach(function(item, i, arr){
+            item.classList.remove('darkened')
+        });
+    })
+});
+    
+        
+  
